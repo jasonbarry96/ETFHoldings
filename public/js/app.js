b2ff8853c8c735d8658a4847b6e1dc47a4d2ca58
jasonbarry96 <jasonbarry96@gmail.com>
2022-01-17 17:07:43 -0500

    Added ability to upload a CSV of ETFs

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,9 @@ console.log("Client side JS file loaded");
 document.getElementById("etfinput").addEventListener("submit", function(event){
   event.preventDefault()
 });
+document.getElementById("csvinput").addEventListener("submit", function(event){
+  event.preventDefault()
+});
 
 async function postData(data = {}) {
   // Default options are marked with *
@@ -29,7 +32,51 @@ async function postData(data = {}) {
     body: JSON.stringify(formatted) // body data type must match "Content-Type" header
   }).then(response => response.json())
     .then(data => element.innerHTML = "<h2>ETF Holdings</h2> <div id='constituents'>" + data + "</div>");
-  // const result = item => <p>
-  // const results = response.json();
-  // return results; // parses JSON response into native JavaScript objects
+}
+
+async function csvPostData(data = {}) {
+  // Default options are marked with *
+  const element = document.getElementById('constituents');
+  element.innerHTML = "<h2>ETF Holdings</h2>";
+  const reader = new FileReader();
+  const input = data.files[0];
+  reader.onload = function(event) {
+    // console.log(event.target.result);
+    const csv = event.target.result;
+    const text = csv.slice(csv.indexOf("\n") + 1).split("\r\n");
+    var i = 0;
+    var holding, allHoldings="";
+    for (i in text){
+      // element.innerHTML = element.innerHTML + "<div>" + text[i] + ": <br>";
+      callPost(text[i]).then((data, text, i) => {holding = data.join();
+                                     // console.log(holding);
+                                     //allHoldings.concat(holding);
+                                     element.innerHTML = element.innerHTML + "<div>" + holding + "</div> <br>";
+                                     // element.innerHTML = element.innerHTML + "<div>" + text[i] + ": " + holding + "</div> <br>";
+                                     console.log(element.innerHTML);
+                                    })
+    }
+
+    // console.log(text);
+
+  };
+  reader.readAsText(input);
+}
+
+async function callPost(holding){
+    holding = {etf: holding};
+    const response = await fetch("http://localhost:3000/", {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        //'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(holding) // body data type must match "Content-Type" header
+    });
+    return response.json();
 }
